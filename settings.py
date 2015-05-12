1394b9013fa275bbe8af85eb7bd0339c082fe630
desmond-ong <dco@stanford.edu>
2015-05-12 00:43:02 -0700

    sentiment lexica

--- settings.py
+++ settings.py
@@ -4,5 +4,6 @@ MODEL_ROOT = './word2vec_model'
 DATA_ROOT = './data'
 NGRAM_FNAME = 'bigrams_tokenized'
 JSON_FNAME = 'reviews_Electronics.json'
+SENTIMENT_PATH = './sentiment/'
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
