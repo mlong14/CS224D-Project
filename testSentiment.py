5a63da01c04cb013590f7629dcee9f0591ec3c78
desmond-ong <dco@stanford.edu>
2015-05-28 22:00:38 -0700

    testSentiment

--- testSentiment.py
+++ testSentiment.py
@@ -7,6 +7,7 @@ print(myAnnotator.annotate('awesome'))
 
 import os
 
+#testCommand = 'java -cp stanford-corenlp-full-2013-11-12/"*" -mx5g edu.stanford.nlp.sentiment.SentimentPipeline -file test_review.txt > test_sentiment_output.txt'
 testCommand = 'java -cp stanford-corenlp-full-2013-11-12/"*" -mx5g edu.stanford.nlp.sentiment.SentimentPipeline -file test_review.txt'
 os.system(testCommand)
 
