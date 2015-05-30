166b458c2178366a3d809b4ec8edb2bf6669031d
EC2 Default User <ec2-user@ip-10-43-135-185.us-west-2.compute.internal>
2015-05-30 07:10:16 +0000

    discoverAspects.py takes in a list of seed aspects and generates top-10 results

--- discoverAspects.py
+++ discoverAspects.py
@@ -15,23 +15,20 @@ import numpy as np
 
 logging.basicConfig(level=logging.INFO)
 
-def discAspects():
+def discAspects(inputFile):
     model = gensim.models.word2vec.Word2Vec.load(os.path.join(settings.MODEL_ROOT,'word2vec_cbow_size300_window10_mincount40_sample1e-3'))
 
-    # while (1):
-    #     print "Enter query"
-    #     query = raw_input()
-    #     try:
-    #         print model.most_similar(positive=query.split())
-    #     except (KeyError):
-    #         logging.info("word not found")
-    #         pass
-    #print model.most_similar(positive=['build','quality'])
-    #print model.most_similar(positive=['screen','quality'])
-    #print model.most_similar(positive=['macbook'])
-    #print model.most_similar(positive=['quality'])
+    # Read input file
+    with open(inputFile) as f:
+        for line in f:
+            try:
+                print line.split()
+                print model.most_similar(positive=line.split())
+            except (KeyError):
+                print("word %s not found"%(line.split()))
+                pass
 def main():
-    discAspects()
+    discAspects(sys.argv[1])
 
 if __name__ == '__main__':
     main()
