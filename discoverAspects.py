b3e19b2a26f0cee405b2520739df7fa5d32aa765
desmond-ong <dco@stanford.edu>
2015-06-02 22:16:25 -0700

    poster updates

--- discoverAspects.py
+++ discoverAspects.py
@@ -15,7 +15,7 @@ import numpy as np
 
 logging.basicConfig(level=logging.INFO)
 
-def discAspects(inputFile):
+def discAspects(inputFile, numClosest=5):
     model = gensim.models.word2vec.Word2Vec.load(os.path.join(settings.MODEL_ROOT,'word2vec_cbow_size300_window10_mincount40_sample1e-3'))
 
     # Read input file
@@ -23,19 +23,20 @@ def discAspects(inputFile):
         for line in f:
             try:
                 print line.split()
-                print model.most_similar(positive=line.split())
-                thisList = model.most_similar(positive=line.split())
+                print model.most_similar(positive=line.split(), topn=numClosest)
+                thisList = model.most_similar(positive=line.split(), topn=numClosest)
                 with open('aspectDictionary.txt','a') as f:
                     f.write(" ".join(line.split()) + "\n")
                     for item in thisList:
-                        f.write(item[0].rstrip(',') + "\n")
+                        #f.write(item[0].rstrip(',') + "\n")
+                        f.write(item[0] + "\n")
             except (KeyError):
                 print("word %s not found"%(line.split()))
                 pass
 def main():
     with open('aspectDictionary.txt','w') as f:
         f.write("")
-    discAspects(sys.argv[1])
+    discAspects(sys.argv[1], 5)
 
 if __name__ == '__main__':
     main()
