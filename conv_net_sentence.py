cb40b2f9e51464dea8dc01f5bd8027467e330d42
EC2 Default User <ec2-user@ip-10-90-1-95.us-west-2.compute.internal>
2015-06-03 04:11:27 +0000

    Fixed number of labels

--- conv_net_sentence.py
+++ conv_net_sentence.py
@@ -184,6 +184,11 @@ def train_conv_net(datasets,
             best_val_perf = val_perf
             test_loss = test_model_all(test_set_x,test_set_y)
             test_perf = 1- test_loss
+
+    # dump model to file
+    with open('model.zip', 'w') as f:
+        pickle.dump(classifier, f)
+
     return test_perf
 
 def shared_dataset(data_xy, borrow=True):
@@ -289,7 +294,7 @@ def make_idx_data_cv(revs, word_idx_map, cv, max_l=51, k=300, filter_h=5):
 
 if __name__=="__main__":
     print "loading data...",
-    x = pickle.load(open("mr.p_5","rb"))
+    x = pickle.load(open("mr.p_toy","rb"))
     revs, W, W2, word_idx_map, vocab = x[0], x[1], x[2], x[3], x[4]
     print "data loaded!"
     mode= sys.argv[1]
@@ -311,13 +316,12 @@ if __name__=="__main__":
     r = range(0,10)
     for i in r:
         datasets = make_idx_data_cv(revs, word_idx_map, i, max_l=56,k=300, filter_h=5)
-        pdb.set_trace()
         perf = train_conv_net(datasets,
                               U,
                               lr_decay=0.95,
                               filter_hs=[3,4,5],
                               conv_non_linear="relu",
-                              hidden_units=[100,2],
+                              hidden_units=[100,3],
                               shuffle_batch=True,
                               n_epochs=1,
                               sqr_norm_lim=9,
